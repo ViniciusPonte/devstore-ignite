df33a5b221527fcb79b9df09b2b7a2233f1f395f
Vinicius Ponte <viniciusbponte@gmail.com>
2023-11-15 11:54:44 -0300

    fix build boundaries

---
+++ src/app/(store)/search/current-search.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+export function CurrentSearch() {
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <p>
+      Resultados para: <span className="font-semibold">{query}</span>
+    </p>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Cart } from './cart'
 import { SearchForm } from './search-form'
+import { Suspense } from 'react'
 
 export function Header() {
   return (
@@ -11,7 +12,9 @@ export function Header() {
           devstore
         </Link>
 
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
       <div className="flex items-center gap-4">
         <Cart />
